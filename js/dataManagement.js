e76b9ab0ba41839f295f359736f5e8b53167893a
Alexander Stach <stach.alex@yahoo.de>
2026-07-23 21:08:23 +0200

    Import von JSON Dateien
    Der Import wurde nun ebenfalls implementiert einschließlich simpler Validierung.
    
    #8

--- js/dataManagement.js
+++ js/dataManagement.js
@@ -1,6 +1,38 @@
-function importJSON(){
-    console.log("Daten werden importiert");
-};
+function importJSON() {
+    const input = document.createElement("input");
+    const reader = new FileReader();
+
+    input.type = "file";
+    input.accept = ".json";
+
+    input.addEventListener("change", () => {
+        const file = input.files[0];
+
+        if (!file) {
+            return;
+        }
+
+        reader.onload = () => {
+            try {
+                const data = JSON.parse(reader.result);
+
+                validateJSON(data);
+
+                applications = data;
+                saveApplications();
+                loadDashboard();
+
+            } catch (error) {
+                alert(error.message);
+                console.error(error);
+            }
+        };
+
+        reader.readAsText(file);
+    });
+
+    input.click();
+}
 
 function exportJSON() {
     const json = JSON.stringify(applications, null, 4);
@@ -19,4 +51,25 @@ function exportJSON() {
     link.click();
 
     URL.revokeObjectURL(url);
+};
+
+function validateJSON(data) {
+
+    if (!Array.isArray(data)) {
+        throw new Error("Die JSON-Datei enthält keine Liste von Bewerbungen.");
+    }
+
+    const isValid = data.every(application => {
+        return (
+            typeof application.id === "number" &&
+            typeof application.company === "string" &&
+            typeof application.position === "string" &&
+            typeof application.status === "string"
+        );
+    });
+
+    if (!isValid) {
+        throw new Error("Die Datei besitzt ein ungültiges Format.");
+    }
+
 }
